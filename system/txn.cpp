85ba591a699c42dd55716e13178c9493f07306d7
xiangyiwen <xyw990331>
2022-11-24 14:29:55 +0800

    No-WAIT Version: Fix a bug. This bug prevents the warmup to finish.

--- system/txn.cpp
+++ system/txn.cpp
@@ -459,7 +459,8 @@ row_t * txn_man::get_row(row_t * row, access_t type) {
     #elif CC_ALG == IC3
         return accesses[row_cnt - 1]->data;
     #elif CC_ALG == SLER
-        return accesses[row_cnt - 1]->data;
+//        return accesses[row_cnt - 1]->data;
+        return row;
     #else
       return accesses[row_cnt - 1]->data;
     #endif
